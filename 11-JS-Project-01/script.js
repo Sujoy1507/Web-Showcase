577fd65918957922be3e0f0fd29ea3b4e51cba9b
Sujoy Saha <ace.sujoy@gmail.com>
2025-12-17 19:18:29 +0530

    open Features

--- 11-JS-Project-01/script.js
+++ 11-JS-Project-01/script.js
@@ -1,17 +1,21 @@
-const allElems = document.querySelectorAll(".elem");
-const allfullElemPage = document.querySelectorAll(".fullElem");
-const fulPageBackBtn = document.querySelectorAll(".fullElem .back-container .container");
+function openFeatuers() {
+    const allElems = document.querySelectorAll(".elem");
+    const allfullElemPage = document.querySelectorAll(".fullElem");
+    const fulPageBackBtn = document.querySelectorAll(
+        ".fullElem .back-container .container"
+    );
 
-allElems.forEach((elem) => {
-    elem.addEventListener("click", () => {
-        allfullElemPage[elem.id].style.display = "block";
+    allElems.forEach((elem) => {
+        elem.addEventListener("click", () => {
+            allfullElemPage[elem.id].style.display = "block";
+        });
     });
-});
 
+    fulPageBackBtn.forEach((back) => {
+        back.addEventListener("click", () => {
+            allfullElemPage[back.id].style.display = "none";
+        });
+    });
+}
 
-fulPageBackBtn.forEach(back=>{
-    back.addEventListener('click',()=>{
-        allfullElemPage[back.id].style.display = "none";
-        
-    })
-})
+openFeatuers();
