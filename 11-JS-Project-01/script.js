12bc4c0f27f7345d7aa4bc4580c13555f5134390
Sujoy Saha <ace.sujoy@gmail.com>
2025-12-17 20:19:45 +0530

    date feature

--- 11-JS-Project-01/script.js
+++ 11-JS-Project-01/script.js
@@ -19,3 +19,32 @@ function openFeatuers() {
 }
 
 openFeatuers();
+
+// Date featuers
+
+function dateFeatuers() {
+    const dayInput = document.querySelector(".date-container .day span");
+    const monthInput = document.querySelector(".date-container .month span");
+    const dateInput = document.querySelector(".date-container .date span");
+    const timeInput = document.querySelector(".date-container .time span");
+
+    dayInput.textContent = new Date().toLocaleDateString("en-US", {
+        weekday: "long",
+    });
+    monthInput.textContent = new Date().toLocaleDateString("en-US", {
+        month: "long",
+    });
+    dateInput.textContent = new Date().toLocaleDateString("en-US", {
+        day: "numeric",
+        month: "numeric",
+        year: "numeric",
+    });
+    timeInput.textContent = new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true,
+    });
+}
+
+dateFeatuers();
